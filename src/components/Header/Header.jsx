a90f1ab48a0ee7125d6ee07adfdf8e6dfbf17617
Maksudul <maaksuduldish@gmail.com>
2025-04-12 10:35:50 +0600

    components setup and headers,banner added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,22 @@
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+
+    const navLinks = <>
+        <NavLink className="btn-info mr-5" to="/">Home</NavLink>
+        <NavLink className="btn-info mr-5" to="/signin">Sign In</NavLink>
+        <NavLink className="btn-info mr-5" to="/signup">Sign Up</NavLink>
+    </>
+
     return (
         <div>
             <div className="navbar bg-base-100">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">WebAuth</a>
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Link</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="bg-base-100 rounded-t-none p-2">
-                                    <li><a>Link 1</a></li>
-                                    <li><a>Link 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
+                        {navLinks}
                     </ul>
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Home page</h1>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                        className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">WEB AUTH by Firebase</h1>
+                        <p className="py-6">
+                            It's all about Firebase authentication project. Please Sign Up and Sign In to become a member of WebAuth.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
