bca47029619f0708cd821570f06e1dd13f14ee91
Maksudul <maaksuduldish@gmail.com>
2025-04-12 12:53:23 +0600

    Sign up and sign in done using firebase

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,5 +1,27 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../assets/firebase/firebase.init";
+import { useState } from "react";
 
 const SignIn = () => {
+
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
+
+    const handleSignIn = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                console.log(result.user);
+                setSuccess('User Signed In successfully !')
+                setError('');
+            })
+            .catch((error) => {
+                setError(error.message)
+            })
+    }
+
     return (
         <div>
             <div className="hero bg-base-200 min-h-screen">
@@ -12,11 +34,17 @@ const SignIn = () => {
                     </div>
                     <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
                         <div className="card-body">
-                            <form className="fieldset">
+                            <form onSubmit={handleSignIn} className="fieldset">
                                 <label className="fieldset-label">Email</label>
-                                <input type="email" name="email" className="input w-full" placeholder="Email" />
+                                <input type="email" name="email" className="input w-full" placeholder="Email" required />
                                 <label className="fieldset-label">Password</label>
-                                <input type="password" name="password" className="input w-full" placeholder="Password" />
+                                <input type="password" name="password" className="input w-full" placeholder="Password" required />
+                                {
+                                    error ?
+                                        <p className="font-bold text-red-500">{error}</p>
+                                        :
+                                        <p className="font-bold text-green-500">{success}</p>
+                                }
                                 <button className="btn btn-neutral mt-4">Sign In</button>
                             </form>
                         </div>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,11 +1,36 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../assets/firebase/firebase.init";
+import { useState } from "react";
 
 const SignUp = () => {
+
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
 
     const handleSignUp = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+
+        if (password < 6) {
+            setError('Password should be 6 characters')
+            return;
+        }
+        else if (!/[A-Z]/.test(password)) {
+            setError('Password at least one Uppercase')
+            return;
+        }
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                console.log(result.user);
+                setError('');
+                setSuccess("Account created successfully");
+            })
+            .catch((error) => {
+                console.log(error);
+                setError(error.message);
+            })
     }
 
     return (
@@ -22,11 +47,17 @@ const SignUp = () => {
                         <div className="card-body">
                             <form onSubmit={handleSignUp} className="fieldset">
                                 <label className="fieldset-label">Name</label>
-                                <input type="text" name="name" className="input w-full" placeholder="name" />
+                                <input type="text" name="name" className="input w-full" placeholder="name" required />
                                 <label className="fieldset-label">Email</label>
-                                <input type="email" name="email" className="input w-full" placeholder="Email" />
+                                <input type="email" name="email" className="input w-full" placeholder="Email" required />
                                 <label className="fieldset-label">Password</label>
-                                <input type="password" name="password" className="input w-full" placeholder="Password" />
+                                <input type="password" name="password" className="input w-full" placeholder="Password" required />
+                                {
+                                    error ? <div>
+                                        <p className="text-red-500 font-bold">{error}</p>
+                                    </div> :
+                                        <div><p className="text-green-500 font-bold">{success}</p></div>
+                                }
                                 <button className="btn btn-neutral mt-4">Sign Up</button>
                             </form>
                         </div>
